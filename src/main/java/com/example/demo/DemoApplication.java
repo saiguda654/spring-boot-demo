92e92f33f4f63737bf73f3f226d1bdf7e1b5237d
saiguda654 <132733120+saiguda654@users.noreply.github.com>
2024-11-19 22:51:25 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
+@RestController
+public class HelloController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, Welcome to My Web Application!";
+    }
 }
